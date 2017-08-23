09485706a30129db9d29d40de7b354f35fb61ad5
apuzyrevsky <mfsaib0t@gmail.com>
2017-08-23 19:42:02 +0300

    Finished two exercises

--- Chapter15/Chapter15/Program.cs
+++ Chapter15/Chapter15/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Chapter15
 {
@@ -10,6 +11,52 @@ namespace Chapter15
     {
         static void Main(string[] args)
         {
+            string filename = @"..\..\sample.txt";
+            string firstFile = @"..\..\firstfile.txt";
+            string secondFile = @"..\..\secondfile.txt";
+            string result = @"..\..\result.txt";
+
+        }
+
+        public static void SaveTextFromOneFileToAnother(string filename, string result)
+        {
+
+            StreamReader reader = new StreamReader(filename);
+            StreamWriter writer = new StreamWriter(result, true);
+            int lineNumber = 0;
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                lineNumber++;
+                writer.WriteLine(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+            writer.Close();
+        }
+
+        public static void PrintOddLines(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+            int lineNumber = 0;
+            string line = reader.ReadLine();
+            using (reader)
+            {
+                while (line != null)
+                {
+                    lineNumber++;
+                    if (IsOdd(lineNumber))
+                    {
+                        Console.WriteLine(line);
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+        }
+
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
         }
     }
 }
